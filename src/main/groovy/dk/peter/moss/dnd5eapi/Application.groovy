18737d42c043835c5ea5b5d585dcbba4285161be
Peter K. Moss <peter@moss.dk>
2022-10-12 18:08:16 +0200

    add spring boot initial application

---
+++ src/main/groovy/dk/peter/moss/dnd5eapi/Application.groovy
@@ -0,0 +1,13 @@
+package dk.peter.moss.dnd5eapi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(Application, args)
+    }
+
+}
